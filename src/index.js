0b663a24557768ca3b5955de7823662f428f7fd4
BogdanLyopa <bogdanlyopa@gmail.com>
2021-02-07 21:03:48 +0200

    add promises

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles.css';
+// import './js/promise-1';
+// import './js/promise-2';
+// import './js/promise-3';
+import taskOne from './js/promise-1';
+import taskTwo from './js/promise-2';
+import taskThree from './js/promise-3';
+const refs = {
+  btnOne: document.querySelector('#task-1'),
+  btnTwo: document.querySelector('#task-2'),
+  btnThree: document.querySelector('#task-3'),
+};
+refs.btnOne.addEventListener('click', taskOne);
+refs.btnTwo.addEventListener('click', taskTwo);
+refs.btnThree.addEventListener('click', taskThree);
